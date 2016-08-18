020ef2781b6238b85dfd20f3d11ae14cc6df9983
Albrecht Beyle <albrecht.beyle@citrix.com>
2016-08-18 13:05:52 +0200

    admin UI

---
+++ admin-ui/src/main/java/com/cloudnative/admin/Application.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2000-2016 Citrix Online LLC
+ * All Rights Reserved Worldwide.
+ *
+ * THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO CITRIX ONLINE
+ * AND CONSTITUTES A VALUABLE TRADE SECRET.  Any unauthorized use,
+ * reproduction, modification, or disclosure of this program is
+ * strictly prohibited.  Any use of this program by an authorized
+ * licensee is strictly subject to the terms and conditions,
+ * including confidentiality obligations, set forth in the applicable
+ * License and Co-Branding Agreement between Citrix Online LLC and
+ * the licensee.
+ */
+package com.cloudnative.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@EnableDiscoveryClient
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
